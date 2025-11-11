c570b0831ec1a7b750196552b95d657b428d958d
William Bautista <williambautista@gmail.com>
2025-11-11 18:10:59 +0000

    Adjust structure for i18n

---
+++ src/i18n/en.ts
@@ -0,0 +1,22 @@
+export default {
+  about: {
+    text: 'We are a Latin American Garage trying to disrupt the LowRider and bikes custom paint.',
+    title: 'About Us'
+  },
+  code: 'en',
+  contactUs: {
+    email: 'Email Us',
+    location: 'Our location',
+    phone: 'Call Us'
+  },
+  languageButton: 'Switch to Spanish 🇨🇴',
+  meta: {
+    description:
+      'Latin American Garage specializing in LowRider and custom bike paint.'
+  },
+  paymentButton: 'Make a Payment',
+  socialNetworks: {
+    instagram: 'Follow us on Instagram',
+    tiktok: 'Follow us on TikTok'
+  }
+}
---
+++ src/i18n/es.ts
@@ -0,0 +1,22 @@
+export default {
+  about: {
+    text: 'Somos un Garage Latinoamericano tratando de revolucionar el pintado personalizado de LowRiders y motos.',
+    title: 'Sobre Nosotros'
+  },
+  code: 'es',
+  contactUs: {
+    email: 'Envíanos un email',
+    location: 'Nuestra ubicación',
+    phone: 'Llámanos'
+  },
+  languageButton: 'Cambiar a Inglés 🇺🇸',
+  meta: {
+    description:
+      'Garage Latinoamericano especializado en LowRiders y pintado personalizado de motos.'
+  },
+  paymentButton: 'Realizar un Pago',
+  socialNetworks: {
+    instagram: 'Síguenos en Instagram',
+    tiktok: 'Síguenos en TikTok'
+  }
+}
