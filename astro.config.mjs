ba503e983721eaaddac5edf57200a6b1fcbc445f
William Bautista <williambautista@gmail.com>
2025-11-23 21:46:23 +0000

    [SSG-10] Adjust-custom-domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/',
+  base: '/es',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
