ced407598af0c32318653540b5c793d4d613c126
William Bautista <williambautista@gmail.com>
2025-11-23 21:13:39 +0000

    [SSG-10] Adjust-custom-domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/steel-and-soul-garage',
+  base: '/',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
